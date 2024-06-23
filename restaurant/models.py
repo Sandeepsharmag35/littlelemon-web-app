2c2e5630a2b0307d1b57db851ec9e6255813be9f
Sandeepsharmag35 <72307548+Sandeepsharmag35@users.noreply.github.com>
2024-06-23 12:20:14 +0545

    Added Files

---
+++ restaurant/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+class Booking(models.Model):
+    first_name = models.CharField(max_length=200)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self): 
+        return self.first_name
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+   name = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   menu_item_description = models.TextField(max_length=1000, default='') 
+   image = models.ImageField(upload_to="menu_items", blank=True)
+
+   def __str__(self):
+      return self.name
