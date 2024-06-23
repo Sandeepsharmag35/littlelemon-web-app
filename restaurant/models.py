ff46bf2eac39724c2bfcdcf025c191c43a4060e5
Sandeepsharmag35 <72307548+Sandeepsharmag35@users.noreply.github.com>
2024-06-23 12:48:19 +0545

    Updated

--- restaurant/models.py
+++ restaurant/models.py
@@ -14,7 +14,7 @@ class Booking(models.Model):
 # Add code to create Menu model
 class Menu(models.Model):
    name = models.CharField(max_length=200) 
-   price = models.IntegerField(null=False) 
+   price = models.DecimalField(max_digits=10, null=False, decimal_places=2) 
    menu_item_description = models.TextField(max_length=1000, default='') 
    image = models.ImageField(upload_to="menu_items", blank=True)
 
